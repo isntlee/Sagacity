0790294d3122a7f1bbfb33d1685460c1599f0a75
Space-Rx <leedavidmartina@gmail.com>
2020-05-27 13:47:24 +0000

    testing fetch() working through global variable

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, redirect, request, url_for, session, abort, flash, jsonify,json
+from flask import Flask, render_template, redirect, request, url_for, session, abort, flash, jsonify, json
 from flask_pymongo import PyMongo, pymongo
 from bson.objectid import ObjectId
 import bcrypt
