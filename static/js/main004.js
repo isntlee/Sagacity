52aab903c7d8946cf1f9b9829ddf4dce2ec808fd
Space-Rx <leedavidmartina@gmail.com>
2020-06-18 22:47:59 +0000

    Added sweetAlert .js

--- static/js/main004.js
+++ static/js/main004.js
@@ -6,3 +6,7 @@
     var instances = M.FormSelect.init(elems, {});
     // I'm probably not going to leave this as is, this is a mess
   });
+
+  function sweetAlert () {
+      swal("Here's the title!", "...and here's the text!");
+  }
