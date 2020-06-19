b4e0cea849a79bb2b5f8cec97d4e068e711b6b9d
Space-Rx <leedavidmartina@gmail.com>
2020-06-19 12:42:56 +0000

    Rating/likes system working, more fine-tuning to come

--- static/js/main004.js
+++ static/js/main004.js
@@ -7,6 +7,31 @@
     // I'm probably not going to leave this as is, this is a mess
   });
 
-  function sweetAlert () {
-      swal("Here's the title!", "...and here's the text!");
-  }
+
+  function sweetAlert_notLogged () {
+      swal({title: "Little problem",
+            text: "Have to log-in first",
+            icon: "warning",
+            timer: 1500,
+            imageUrl: 'https://lionalert.org/wp-content/uploads/2020/01/Lion-Cubs-1080x675.jpg'
+
+            });
+        }
+
+    function changeColor_liked() { 
+        document.getElementById("up-arrow-liked-inSession").style.color = "red";
+    }
+
+    function changeColor_disliked() { 
+        document.getElementById("down-arrow-disliked-inSession").style.color = "blue"; 
+    }
+
+
+    function sweetAlert_notHere () {
+      swal({title: "",
+            text: "You can only vote in the full story page",
+            icon: "warning",
+            timer: 1500,
+            imageUrl: 'https://lionalert.org/wp-content/uploads/2020/01/Lion-Cubs-1080x675.jpg'
+            });
+        }
