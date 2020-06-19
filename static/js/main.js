b14413645bd7a49eb6f382b8cdc690c99fc214e0
Space-Rx <leedavidmartina@gmail.com>
2020-06-19 12:49:55 +0000

    Fixing CSS issues

--- static/js/main.js
+++ static/js/main.js
@@ -8,16 +8,6 @@
   });
 
 
-  function sweetAlert_notLogged () {
-      swal({title: "Little problem",
-            text: "Have to log-in first",
-            icon: "warning",
-            timer: 1500,
-            imageUrl: 'https://lionalert.org/wp-content/uploads/2020/01/Lion-Cubs-1080x675.jpg'
-
-            });
-        }
-
     function changeColor_liked() { 
         document.getElementById("up-arrow-liked-inSession").style.color = "red";
     }
@@ -27,11 +17,18 @@
     }
 
 
+  function sweetAlert_notLogged () {
+      swal({title: "Little problem",
+            text: "Have to log-in first",
+            icon: "warning",
+            timer: 1500
+            });
+        }
+
     function sweetAlert_notHere () {
       swal({title: "",
             text: "You can only vote in the full story page",
             icon: "warning",
-            timer: 1500,
-            imageUrl: 'https://lionalert.org/wp-content/uploads/2020/01/Lion-Cubs-1080x675.jpg'
+            timer: 1500
             });
         }
