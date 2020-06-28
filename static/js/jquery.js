7255637dd667a951ef63f51b112a8b5bd66ea90f
Space-Rx <leedavidmartina@gmail.com>
2020-06-28 12:23:17 +0000

    Adding navbar transitions

---
+++ static/js/jquery.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function () {
+      $('select').formSelect();
+      $('.materialboxed').materialbox();
+      });
+      
+      $(document).ready(function () {
+      var selectorvar = $('#selectordiv').html();
+        if (selectorvar != 'true')
+            { $(".nav-wrapper").removeClass("transparent").addClass("white") ;}
+        });
