d0bb030ac48bdd40deb814b4d5efe2165bd7a02b
Space-Rx <leedavidmartina@gmail.com>
2020-08-14 12:22:56 +0000

    CSS final touches

--- static/js/main000.js
+++ static/js/main000.js
@@ -49,3 +49,24 @@
             timer: 2000
             });
         }
+
+
+    function sweetAlert_delete() {
+      swal({
+        title: "Are you sure?",
+        text: "Once deleted, you will not be able to recover this imaginary file!",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true,
+        })
+        
+        .then((confirm) => {
+        if (confirm) {
+            swal("Poof! Your imaginary file has been deleted!", {
+            icon: "success",
+            });
+        } else {
+            swal("Your imaginary file is safe!");
+        }
+        });
+        }
